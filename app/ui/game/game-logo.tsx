77604154f0fe3a7fc2adccc505541747d4d3d01b
Aaron <aaronpromail@gmail.com>
2025-04-27 13:12:26 +0100

    feat: add GameLogo component

---
+++ app/ui/game/game-logo.tsx
@@ -0,0 +1,10 @@
+import { IoEgg } from "react-icons/io5";
+
+export default function GameLogo() {
+    return <>
+        <span className="flex items-center justify-center space-x-2">
+            <IoEgg className="text-4xl text-(--accent-color)" />
+            <h2 className="text-3xl">EggHuntFun</h2>
+        </span>
+    </>
+}
