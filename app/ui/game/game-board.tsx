3d0dcacb2545651286dda4cdfb94e6f9713b75a1
Aaron <aaronpromail@gmail.com>
2025-04-29 21:56:14 +0100

    feat: implement GameBoard component with score tracking and shuffled items

--- app/ui/game/game-board.tsx
+++ app/ui/game/game-board.tsx
@@ -1,14 +1,48 @@
-export default function GameBoard(){
-    return(
-        <div className="grid grid-cols-5 gap-4">
-            {Array.from({ length: 25 }).map((_, index) => (
-                <div
-                    key={index}
-                    className="w-24 h-24 bg-(--light-accent-color) flex items-center justify-center text-white text-xl font-bold rounded-lg"
-                >
-                    🥚
-                </div>
-            ))}
+'use client'
+
+import { useState } from "react";
+import GameCell from "./game-cell";
+
+const items = [
+    "🧐", "🤠", "🥚", "👀", "🙌🏽",
+    "🏃🏽‍♀️‍➡️", "🌝", "🌚", "🥚", "🎱",
+    "🐣", "🐥", "🍗", "🔍", "🥚",
+    "🥚", "🏃🏽‍♂️", "🏃🏽‍♀️", "🥚", "🏃🏾‍♂️‍➡️",
+    "🤷🏽‍♂️", "🤷🏽‍♀️", "🤔", "🐔", "🔜",
+];
+
+function shuffleArray(array: string[]) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+export default function GameBoard() {
+    const [score, setScore] = useState(25);
+    const [gameBoard] = useState(() =>
+        shuffleArray(items).map((item) => ({ item, isRevealed: false }))
+    );
+
+    const handleCellClick = (item: string) => {
+        if (item === "🥚") {
+            console.log("Egg found!");
+        } else {
+            setScore((prevScore) => prevScore - 1);
+        }
+    };
+
+    return (
+        <div>
+            <div className="grid grid-cols-5 gap-4">
+                {gameBoard.map((cell, index) => (
+                    <GameCell
+                        key={index}
+                        item={cell.item}
+                        onClick={() => handleCellClick(cell.item)}
+                    />
+                ))}
+            </div>
+            <div className="mt-4 text-center">
+                <p className="text-lg font-bold">Score: {score}</p>
+            </div>
         </div>
-    )
+    );
 }
