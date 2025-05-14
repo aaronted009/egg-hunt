b3c31926ffbc85efbd1d4130dd1fc050f42a5acd
Aaron <aaronpromail@gmail.com>
2025-05-14 13:08:46 +0100

    :broom: chore: wrap GameLogo in Link
    - change GameLogo to use Link for navigation
    - improves user experience by making the logo clickable

--- app/ui/game/game-logo.tsx
+++ app/ui/game/game-logo.tsx
@@ -1,12 +1,13 @@
 import { IoEgg } from "react-icons/io5";
+import Link from "next/link";
 
 export default function GameLogo() {
   return (
-    <>
+    <Link href="/">
       <span className="flex items-center justify-center space-x-1/2">
         <IoEgg className="text-3xl text-(--accent-color)" />
         <h2 className="text-2xl font-normal">EggHuntFun</h2>
       </span>
-    </>
+    </Link>
   );
 }
